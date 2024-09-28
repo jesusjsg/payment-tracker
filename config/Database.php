698a13e8b9bd9ef8397b5b3ad034523e4864d91e
jesus <jesusjsg@proton.me>
2024-09-28 01:46:59 -0400

    feat: new class for Database connection

---
+++ config/Database.php
@@ -0,0 +1,36 @@
+<?php
+
+class Database
+{
+    private $host;
+    private $db;
+    private $user;
+    private $password;
+    private $charset;
+
+    public function __construct()
+    {
+        $this->host = $_ENV['DB_HOST'];
+        $this->db = $_ENV['DB_NAME'];
+        $this->user = $_ENV['DB_USER'];
+        $this->password = $_ENV['DB_PASSWORD'];
+        $this->charset = $_ENV['DB_CHARSET'];
+    }
+
+    public function connection()
+    {
+        try {
+            $connection = 'mysql:host=' . $this->host . ';dbname=' . $this->db . ';charset=' . $this->charset;
+            $config = [
+                PDO::ATTR_ERRMODE           => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_EMULATE_PREPARES  => false
+            ];
+            $pdo = new PDO($connection, $this->user, $this->password, $config);
+            return $pdo;
+            
+        } catch (PDOException $error) {
+            throw new Error('Error connection: ' . $error->getMessage());
+        }
+    }
+
+}

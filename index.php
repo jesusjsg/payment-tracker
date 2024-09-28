3f577761b768dc9bfc6f96ae27a248825b66315b
jesus <jesusjsg@proton.me>
2024-09-28 01:48:42 -0400

    perf: cors headers and library dotenv added

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With");
+
 error_reporting(E_ALL);
 ini_set('ignore_repeated_errors', true);
 ini_set('display_errors', false);
@@ -7,6 +11,12 @@ ini_set('log_errors', true);
 ini_set('error_log', '/var/www/html/payment-tracker/php-error.log');
 error_log('App init');
 
+require_once 'vendor/autoload.php';
 require_once 'libs/app.php';
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
 $app = new App();

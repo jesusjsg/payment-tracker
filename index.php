f8a457d16f880dff0078d9c78f5b8973640f01ef
jesus <jesusjsg@proton.me>
2024-10-02 21:26:49 -0400

    perf: all require were removed

--- index.php
+++ index.php
@@ -6,15 +6,15 @@ header("Access-Control-Allow-Headers: X-Requested-With");
 
 error_reporting(E_ALL);
 ini_set('ignore_repeated_errors', true);
-ini_set('display_errors', false);
+ini_set('display_errors', true);
 ini_set('log_errors', true);
 ini_set('error_log', '/var/www/html/payment-tracker/php-error.log');
 error_log('App init');
 
 require_once 'vendor/autoload.php';
-require_once 'libs/app.php';
 
 use Dotenv\Dotenv;
+use App\Core\App;
 
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();

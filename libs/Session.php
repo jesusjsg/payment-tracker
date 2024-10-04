dc71f7c5bfc10da37f24e2614e00f58bffef95ae
jesus <jesusjsg@proton.me>
2024-10-04 18:40:07 -0400

    feat: session class to login

---
+++ libs/Session.php
@@ -0,0 +1,34 @@
+<?php
+
+class Session
+{
+    private $sessionName = 'user';
+
+    public function __construct()
+    {
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+    }
+
+    public function setCurrentSession($user)
+    {
+        $_SESSION[$this->sessionName = $user];
+    }
+
+    public function getCurrentSession()
+    {
+        return $_SESSION[$this->sessionName];
+    }
+
+    public function closeSession()
+    {
+        session_unset();
+        session_destroy();
+    }
+
+    public function exists()
+    {
+        return isset($_SESSION[$this->sessionName]);
+    }
+}

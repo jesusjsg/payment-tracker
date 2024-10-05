9224b773e4ed8ce8adfb39cc6669292198f7219a
jesus <jesusjsg@proton.me>
2024-10-04 22:55:40 -0400

    perf: tables to manage a RBAC
    Role based access control

--- schema/schema.sql
+++ schema/schema.sql
@@ -16,21 +16,32 @@ create table if not exists users (
 
 create table if not exists roles (
     role_id integer unsigned primary key auto_increment,
-    name varchar(40) not null
+    roles_name varchar(40) not null unique
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 create table if not exists categories (
     category_id integer unsigned primary key auto_increment,
     user_id integer unsigned,
-    name varchar(50) unique
+    category_name varchar(50) unique
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 create table if not exists expenses (
     expense_id integer unsigned primary key auto_increment,
     category_id integer unsigned,
     user_id integer unsigned,
-    name varchar(200) unique,
+    expense_name varchar(200) unique,
     amount float,
     create_at timestamp not null default current_timestamp,
     modified_at timestamp not null default current_timestamp on update current_timestamp
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+create table if not exists permissions (
+    permission_id integer unsigned primary key auto_increment,
+    permission_name varchar(100) not null unique
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+create table if not exists role_permissions (
+    role_id integer unsigned,
+    permission_id integer unsigned,
+    primary key (role_id, permission_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;

5c553fba8582d520d3c34b30436fccf5e44fdcc9
jesus <jesusjsg@proton.me>
2024-11-17 19:26:02 -0400

    feat: init error log was removed

--- index.php
+++ index.php
@@ -9,7 +9,6 @@ ini_set('ignore_repeated_errors', true);
 ini_set('display_errors', true);
 ini_set('log_errors', true);
 ini_set('error_log', '/var/www/html/payment-tracker/php-error.log');
-error_log('App init');
 
 require_once 'vendor/autoload.php';
 
